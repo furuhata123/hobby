9db21adba02540120424d80f5ecf17d219541147
furuhata123 <aerobikhttp@outlook.com>
2026-02-19 20:46:08 +0800

    fix path

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+const isDev = process.env.ELEVENTY_RUN_MODE === "serve";
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy({ "public/assets": "assets" });
 
       
     return {
-        pathPrefix:"/hobby/",
+        pathPrefix: isDev ? "/" : "/hobby/",
         dir: {
             input: "src",
             includes: "_includes",
